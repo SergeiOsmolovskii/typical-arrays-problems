530503a330f46e45efaed8e9d203da3c1d0d660b
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2021-07-15 00:20:43 +0300

    feat: add min function, add max function, add avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+  return array.reduce((item, acum) => {
+   return acum + item
+  }, 0) / array.length;
 }
